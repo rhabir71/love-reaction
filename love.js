70b96d8ad864e9dcb0ae5b08bbe317ef731e4fb7
rhabir <rhabir.com>
2022-11-15 19:06:00 +0600

    change text

--- love.js
+++ love.js
@@ -1,23 +1,23 @@
 document.getElementById("yes").addEventListener("click", function () {
-  // document.getElementById("msg").innerText = "I LOVE YOU 💘 ";
-  commnFun2("msg");
+  document.getElementById("msg").innerText = "I LOVE YOU 💘 ";
+  // commnFun2("msg");
 });
 
 function loveLetter() {
-  commnFun2("msg");
-  //   document.getElementById("msg").innerText = "I LOVE YOU 💝 ";
+  // commnFun2("msg");
+    document.getElementById("msg").innerText = "I LOVE YOU SO-MUCH 😄 ";
 }
 
-function commnFun2(id) {
-  document.getElementById(id).innerText = "I LOVE YOU 💘 ";
-}
+// function commnFun2(id) {
+//   document.getElementById(id).innerText = " ";
+// }
 
-function add(a, b) {
-  console.log(a + b);
-}
-function sub(a, b) {
-  console.log(a - b);
-}
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// function sub(a, b) {
+//   console.log(a - b);
+// }
 
-add(10, 20); //arguemnts
-sub(20, 15); //arguments
+// add(10, 20); //arguemnts
+// sub(20, 15); //arguments
